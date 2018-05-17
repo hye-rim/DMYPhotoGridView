dbffd87cff3d772113232a4c25c7770d85b7fdf6
hye-rim <phl2898@gmail.com>
2018-05-17 16:20:44 +0900

    Add RecyclerView, item to MainActivity.xml

--- DMYPhotoGridView-parkhyerim/app/src/main/java/com/hackday/dmyphotogridview_parkhyerim/MainActivity.java
+++ DMYPhotoGridView-parkhyerim/app/src/main/java/com/hackday/dmyphotogridview_parkhyerim/MainActivity.java
@@ -1,9 +1,18 @@
 package com.hackday.dmyphotogridview_parkhyerim;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.MemoryCategory;
 
 public class MainActivity extends BaseActivity{
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private final static int[] ROW_COUNT = {3, 5, 7};
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +20,6 @@ public class MainActivity extends BaseActivity{
         setContentView(R.layout.activity_main);
         init();
 
-        //TODO : 권한체크
         //TODO : glide
 
         //TODO : grid view
@@ -26,7 +34,10 @@ public class MainActivity extends BaseActivity{
     }
 
     private void init(){
+        mContext = getApplicationContext();
+
         checkPermission();
+        Glide.get(this).setMemoryCategory(MemoryCategory.HIGH);
     }
 
     @Override
