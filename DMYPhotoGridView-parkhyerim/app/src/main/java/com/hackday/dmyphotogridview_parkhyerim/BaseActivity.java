d19351d34248e51fb124361f110cdd6e2ebcc313
hye-rim <phl2898@gmail.com>
2018-05-17 16:10:22 +0900

    add checkPermission, add baseActivity.

---
+++ DMYPhotoGridView-parkhyerim/app/src/main/java/com/hackday/dmyphotogridview_parkhyerim/BaseActivity.java
@@ -0,0 +1,87 @@
+package com.hackday.dmyphotogridview_parkhyerim;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by hyerim on 2018. 5. 17....
+ */
+public class BaseActivity extends AppCompatActivity {
+    private final static String TAG = BaseActivity.class.getSimpleName();
+    private static final int REQUEST_READ_STORAGE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    private void init() {
+    }
+
+
+    protected void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestStoragePermission();
+        } else {
+            onPermissionGranted();
+        }
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_STORAGE);
+    }
+
+    protected void onPermissionGranted() {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_READ_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onPermissionGranted();
+                } else {
+                    Toast.makeText(this, "Storage permission is required", Toast.LENGTH_LONG).show();
+                    requestStoragePermission();
+                }
+            }
+        }
+
+    }
+
+}
--- DMYPhotoGridView-parkhyerim/app/src/main/java/com/hackday/dmyphotogridview_parkhyerim/MainActivity.java
+++ DMYPhotoGridView-parkhyerim/app/src/main/java/com/hackday/dmyphotogridview_parkhyerim/MainActivity.java
@@ -3,12 +3,13 @@ package com.hackday.dmyphotogridview_parkhyerim;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
         //TODO : 권한체크
         //TODO : glide
@@ -21,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         //TODO : image list sorting
         //TODO : grouping
         //TODO : pinch zoom
+
+    }
+
+    private void init(){
+        checkPermission();
+    }
+
+    @Override
+    protected void onPermissionGranted() {
+        super.onPermissionGranted();
+
 
     }
 }
